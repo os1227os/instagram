9549d0dde57b6680852552d405360c3ae5e24e83
김대일 <os1227os@naver.com>
2021-05-23 19:25:49 +0900

    5.23

--- main.py
+++ main.py
@@ -50,207 +50,138 @@ from frist import action_function, like_reflection, remove_list
 # 작동 코드 
 ############################################################################################################
 
-
-
-
-#target_list = ["재롱이","아리둥절","홍설","밀키와복이","소녀의행성","몽자"]
-#target_list = ["이서호","유형록"]
-
-
 target_list = []
 tag = []
 
-
-
-print()
-print("==================================")
+print("\n==================================")
 print("Welcome to automatic like system !! ")
-print("==================================")
-print()
-
+print("==================================\n")
 
 
 while True:
-    name = input("사용할 아이디를 입력하세요 : ")
-    print()
-    i = input(f"입력한 아이디가 {name} 이 맞습니까? (맞으면 y , 아니면 n )")
-    print()
+    name = input("사용할 아이디를 입력하세요 : \n")
+    i = input(f"입력한 아이디가 {name} 이 맞습니까? (맞으면 y , 아니면 n )\n")
     if i == "y":
         break
     elif i == "n":
         continue
-    else:
-        print("y 또는 n 만 입력 가능합니다. ( 대소문자 구분 )")
-        print()
+    else: print("y 또는 n 만 입력 가능합니다. ( 대소문자 구분 )\n")
 
 while True:
-    pass_word = input("사용할 비밀번호를 입력하세요 : ")
-    print()
-    p = input(f"입력한 비밀번호가 {pass_word} 가 맞습니까? (맞으면 y , 아니면 n )")
-    print()
+    pass_word = input("사용할 비밀번호를 입력하세요 : \n")
+    p = input(f"입력한 비밀번호가 {pass_word} 가 맞습니까? (맞으면 y , 아니면 n )\n")
     if p == "y":
         break
     elif p == "n":
         continue
     else:
-        print("y 또는 n 만 입력 가능합니다. ( 대소문자 구분 )")
-        print()
+        print("y 또는 n 만 입력 가능합니다. ( 대소문자 구분 )\n")
 
 while True:
     bouns_count = 0
-    usertime = input("2차 비밀번호 인증 시간이 필요합니까? (맞으면 y , 아니면 n )")
-    print()
-    print("참고 : y 선택시 50초 대기시간 , n 선택시 5초 대기시간 적용 !")
-    print()
-    print()
+    usertime = input("2차 비밀번호 인증 시간이 필요합니까? (맞으면 y , 아니면 n )\n")
+    print("참고 : y 선택시 50초 대기시간 , n 선택시 5초 대기시간 적용 !\n\n")
     if bouns_count == 0 and usertime == "y":
-        print("2차 비밀번호 인증 대시기간이 적용 되었습니다. ")
-        print()
+        print("2차 비밀번호 인증 대시기간이 적용 되었습니다. \n")
         break
     elif bouns_count == 0 and usertime == "n":
-        print("2차 비밀번호 인증 대기시간이 적용 되지 않았습니다. ")
-        print()
+        print("2차 비밀번호 인증 대기시간이 적용 되지 않았습니다. \n")
         break
     elif usertime != "y" and usertime != "n":
-        print("y 또는 n 만 입력 가능합니다. ( 대소문자 구분 )")
+        print("y 또는 n 만 입력 가능합니다. ( 대소문자 구분 )\n\n")
         bouns_count += 1
-        print()
-        print()
-
-
-    
-
-
-print()       
-print(" 아이디와 비밀번호가 입력 되었습니다 ! 작업할 종류를 선택해주세요.  ")
-print()
-
+     
+print("\n 아이디와 비밀번호가 입력 되었습니다 ! 작업할 종류를 선택해주세요.  \n")
 
 
 while True:
-    like_type = int(input("1. 팔로워 좋아요 작업    2. 좋아요 반사      3. 둘 다 하기 "))
-    print()
+    like_type = int(input("1. 팔로워 좋아요 작업    2. 좋아요 반사      3. 둘 다 하기 \n"))
     if like_type == 1 or like_type == 2 or like_type == 3:
         if like_type == 1:
-            print("1. 팔로워 좋아요 작업을 선택하셨습니다. ")
-            print()
+            print("1. 팔로워 좋아요 작업을 선택하셨습니다. \n")
             while True:
-                follower_range = input("한 타겟당 몇번 반복 하시겠습니까 ?")
-                print()
+                follower_range = input("한 타겟당 몇번 반복 하시겠습니까 ?\n")
                 try:
                     follower_range = int(follower_range)
-                    print(f"타겟당 {follower_range}번 반복합니다. ")
-                    print()
+                    print(f"타겟당 {follower_range}번 반복합니다. \n")
                     break
                 except:
-                    print("숫자만 입력해주세요. ")
-                    print()
+                    print("숫자만 입력해주세요. \n")
             while True:
-                print(f"현재 타겟 목록 : {target_list}")
-                print()
-                target_append = input("팔로워 작업할 사람 추가 : (종료시 x 입력)")
-                print()
+                print(f"현재 타겟 목록 : {target_list}\n")
+                target_append = input("팔로워 작업할 사람 추가 : (종료시 x 입력)\n")
                 if target_append == "x":
-                    final = input("삭제 하고 싶은 목록이 있습니까? (있으면 y , 없으면 n)")
-                    print()
+                    final = input("삭제 하고 싶은 목록이 있습니까? (있으면 y , 없으면 n)\n")
                     remove_list(final,target_list,like_type)
                     break
                 else:
                     target_list.append(target_append)
 
         elif like_type == 2:
-            print("2. 좋아요 반사 작업을 선택하셨습니다. ")
-            print()
+            print("2. 좋아요 반사 작업을 선택하셨습니다. \n")
             while True:
-                tag_range = input("한 태그당 몇번 반복 하시겠습니까 ?")
-                print()
+                tag_range = input("한 태그당 몇번 반복 하시겠습니까 ?\n")
                 try:
                     tag_range = int(tag_range)
-                    print(f"타겟당 {tag_range}번 반복합니다. ")
-                    print()
+                    print(f"타겟당 {tag_range}번 반복합니다. \n")
                     break
                 except:
-                    print("숫자만 입력해주세요. ")
-                    print()
+                    print("숫자만 입력해주세요. \n")
             while True:
-                print(f"현재 태그 추가 목록 : {tag}")
-                print()
-                append_tag = input("좋아요 작업 할 태그 추가 : (종료시 x 입력)")
-                print()
+                print(f"현재 태그 추가 목록 : {tag}\n")
+                append_tag = input("좋아요 작업 할 태그 추가 : (종료시 x 입력)\n")
                 if append_tag == "x":
-                    final = input("삭제 하고 싶은 목록이 있습니까? (있으면 y , 없으면 n)")
-                    print()
+                    final = input("삭제 하고 싶은 목록이 있습니까? (있으면 y , 없으면 n)\n")
                     remove_list(final,tag,like_type)
                     break
                 else:
                     tag.append(append_tag)
 
         elif like_type == 3:
-            print(" 3. 둘 다 하기를 선택하셨습니다. ")
-            print()
+            print(" 3. 둘 다 하기를 선택하셨습니다. \n")
             time.sleep(1.0)
-            print("팔로워 좋아요 작업 할 타겟 먼저 추가 하겠습니다 . ")
-            print()
-            print()
+            print("팔로워 좋아요 작업 할 타겟 먼저 추가 하겠습니다 . \n\n")
             while True:
-                follower_range = input("한 타겟당 몇번 반복 하시겠습니까 ?")
-                print()
+                follower_range = input("한 타겟당 몇번 반복 하시겠습니까 ?\n")
                 try:
                     follower_range = int(follower_range)
-                    print(f"타겟당 {follower_range}번 반복합니다. ")
-                    print()
+                    print(f"타겟당 {follower_range}번 반복합니다. \n")
                     break
                 except:
-                    print("숫자만 입력해주세요. ")
-                    print()
+                    print("숫자만 입력해주세요. \n")
             while True:
-                print(f"현재 타겟 목록 : {target_list}")
-                print()
-                target_append = input("팔로워 작업할 사람 추가 : (종료시 x 입력)")
-                print()
+                print(f"현재 타겟 목록 : {target_list}\n")
+                target_append = input("팔로워 작업할 사람 추가 : (종료시 x 입력)\n")
                 if target_append == "x":
-                    final = input("삭제 하고 싶은 목록이 있습니까? (있으면 y , 없으면 n)")
-                    print()
+                    final = input("삭제 하고 싶은 목록이 있습니까? (있으면 y , 없으면 n)\n")
                     remove_list(final,target_list,like_type)
                     break
                 else:
-                    target_list.append(target_append)
-            print()
-            print()        
-            print("좋아요 반사할 태그도 추가 하겠습니다. ")
-            print()
+                    target_list.append(target_append)      
+            print("\n\n좋아요 반사할 태그도 추가 하겠습니다. \n\n")
+
             while True:
-                tag_range = input("한 태그당 몇번 반복 하시겠습니까 ?")
-                print()
+                tag_range = input("한 태그당 몇번 반복 하시겠습니까 ?\n")
                 try:
                     tag_range = int(tag_range)
-                    print(f"타겟당 {tag_range}번 반복합니다. ")
-                    print()
+                    print(f"타겟당 {tag_range}번 반복합니다. \n")
                     break
                 except:
-                    print("숫자만 입력해주세요. ")
-                    print()
+                    print("숫자만 입력해주세요. \n")
             while True:
-                print(f"현재 태그 추가 목록 : {tag}")
-                print()
-                append_tag = input("좋아요 작업 할 태그 추가 : (종료시 x 입력)")
-                print()
+                print(f"현재 태그 추가 목록 : {tag}\n")
+                append_tag = input("좋아요 작업 할 태그 추가 : (종료시 x 입력)\n")
                 if append_tag == "x":
-                    final = input("삭제 하고 싶은 목록이 있습니까? (있으면 y , 없으면 n)")
-                    print()
+                    final = input("삭제 하고 싶은 목록이 있습니까? (있으면 y , 없으면 n)\n")
                     now = time.strftime('%m월%d일 %H시:%M분')
                     if final == "y":
                         while True:
-                            select = input("어떤 목록을 지울까요? (x 입력시 종료)")
-                            print()
+                            select = input("어떤 목록을 지울까요? (x 입력시 종료)\n")
                             if select in tag:
-                                print(f"{select} 가 삭제됩니다. ")
-                                print()
+                                print(f"{select} 가 삭제됩니다. \n")
                                 tag_list.remove(select)
-                                print(f"{select} 삭제 완료.")
-                                print()
-                                print(f"현재 목록 : {tag}")
-                                print()
+                                print(f"{select} 삭제 완료.\n")
+                                print(f"현재 목록 : {tag}\n")
                             elif select == "x":
                                 f = open("tag_list.txt",'a')
                                 f.write('\n')
@@ -259,11 +190,8 @@ while True:
                                 f.close()    
                                 break            
                             else:
-                                print("존재 하지 않는 값입니다. 다시 입력해주세요.")
-                                print()
-                                print(tag)
-                                print()
-                                print()
+                                print("존재 하지 않는 값입니다. 다시 입력해주세요.\n")
+                                print(tag,'\n\n')
                     elif final == "n":
                         f = open("tag_list.txt",'a')
                         f.write('\n')
@@ -277,22 +205,18 @@ while True:
     else:
         continue
 
-computer_end = input("모든 준비가 완료 됐습니다 . 작업 후 컴퓨터를 종료 하시겠습니까 ? ( 종료하려면 y , 종료하지 않으려면 n )")
-print()
+computer_end = input("모든 준비가 완료 됐습니다 . 작업 후 컴퓨터를 종료 하시겠습니까 ? ( 종료하려면 y , 종료하지 않으려면 n )\n")
 while True:
     if computer_end == "y" or computer_end == "n":
         if computer_end == "y":
-            print("작업 종료후 컴퓨터를 종료하겠습니다 . 이제 작업을 시작합니다 . ")
-            print()
+            print("작업 종료후 컴퓨터를 종료하겠습니다 . 이제 작업을 시작합니다 . \n")
             break
         else:
-            print("작업 후 컴퓨터를 종료하지 않겠습니다. 이제 작업을 시작합니다 . ")
-            print()
+            print("작업 후 컴퓨터를 종료하지 않겠습니다. 이제 작업을 시작합니다 . \n")
             break
             
     else:
-        computer_end = input("y 또는 n 만 입력 가능합니다 . ")
-        print()
+        computer_end = input("y 또는 n 만 입력 가능합니다 . \n")
 
 
 
@@ -390,16 +314,13 @@ result.writelines(','.join(map(str,all_result)))
 result.close()
 
 
-print("모든 작업이 완료되었습니다 .")
-print()
+print("모든 작업이 완료되었습니다 .\n")
 if computer_end == "y":
-    print("작업 완료후 컴퓨터 종료를 선택하셨습니다 . 3초뒤 컴퓨터를 종료합니다 .")
-    print()
+    print("작업 완료후 컴퓨터 종료를 선택하셨습니다 . 3초뒤 컴퓨터를 종료합니다 .\n")
     for i in range(3,0,-1):
         print(i)
         time.sleep(1.0)
-    print()
-    print("안녕.")
+    print("\n안녕.")
     os.system("shutdown -s")
 
 
